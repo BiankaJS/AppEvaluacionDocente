6a21eed2ad2c1a18e7918a50abe2b7c2b8806fe8
Bianka <biankasarayj@outlook.com>
2024-05-15 12:23:06 -0600

    News pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,20 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'teacher',
+    loadChildren: () => import('./page/teacher/teacher.module').then( m => m.TeacherPageModule)
+  },
+  {
+    path: 'chart',
+    loadChildren: () => import('./page/chart/chart.module').then( m => m.ChartPageModule)
   },
 ];
 
---
+++ src/app/page/login/login.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup = <FormGroup>{};
+
+  background = {
+    backgroundImage:
+      'url(https://img.freepik.com/vector-gratis/ilustracion-plana-dia-maestro-espanol_23-2149347437.jpg?size=338&ext=jpg&ga=GA1.1.1788068356.1715731200&semt=sph)',
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public loadingController: LoadingController
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      password: [null],
+    });
+  }
+
+  signIn() {
+    if (this.loginForm.valid) {
+      setTimeout(() => {
+        this.router.navigate(['home']);
+      }, 2000);
+    } else {
+      console.log('invalid');
+    }
+  }
+
+}
