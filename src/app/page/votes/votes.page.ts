dc7470807561c960ec2c54abc616ec9d3be5715c
Pedro Flores <pedro.r.flores13@gmail.com>
2024-05-16 17:32:01 -0600

    Avance en Podium

--- src/app/page/votes/votes.page.ts
+++ src/app/page/votes/votes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-votes',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotesPage implements OnInit {
 
-  constructor() { }
+  public thirdPlace = { name: 'Alberto Jaramillo', place: "Tercer Lugar", score: 100 };
+  public secondPlace = { name: 'Bianka Juarez', place: "Segundo Lugar", score: 200 };
+  public firstPlace = { name: 'Ashli Acosta', place: "Primer Lugar", score: 300 };
+
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+  logOut() {
+    localStorage.clear()
+    this.route.navigate(['login']);
   }
 
 }
