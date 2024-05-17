80f26d860857daf009031b322020b1f6915070fb
Pedro Flores <pedro.r.flores13@gmail.com>
2024-05-16 19:48:22 -0600

    Mapeo de datos en chart

--- src/app/page/chart/chart.page.ts
+++ src/app/page/chart/chart.page.ts
@@ -11,21 +11,73 @@ import { Observable } from 'rxjs';
 })
 export class ChartPage implements OnInit {
   public teachers: any = [];
+  public questions: any = [];
   teacher: string[] = [];
+  questionIds: number[] = []; // Array to hold question IDs
   constructor(private route: Router, private http: HttpClient) { }
 
   ngOnInit() {
     let url = "./../assets/data/teachers.json";
+    let urlQuestions = "./../assets/data/questions.json";
     let data: Observable<any> = this.http.get(url);
+    let questionsData: Observable<any> = this.http.get(urlQuestions);
+
     data.subscribe(result => {
       this.teachers = result;
 
-    this.teachers.map((t: Teacher) => {
-      console.log(t)
-      console.log(t['first_name'])
-      this.teacher.push(...t['first_name']);
+      // Map the first names of teachers to an array
+      this.teacher = this.teachers.map((t: Teacher) => t.first_name);
     });
-    })
+
+    questionsData.subscribe(result => {
+      this.questions = result;
+
+      // Map the question IDs to an array
+      this.questionIds = this.questions.map((q: any) => q.id);
+
+      // Initialize options after data is loaded
+      this.options = {
+        legend: {
+          data: this.teacher
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        yAxis: [
+          {
+            type: 'category',
+            axisTick: { show: false },
+            data: this.questionIds // Use the question IDs here
+          }
+        ],
+        series: this.generateSeriesData() // Call a function to generate series data
+      };
+    });
+  }
+
+  // Function to generate series data based on teacher information
+  generateSeriesData(): any[] {
+    let seriesData: any[] = [];
+    this.teachers.forEach((teacher: Teacher, index: number) => {
+      seriesData.push({
+        name: teacher.first_name,
+        type: 'bar',
+        label: {
+          show: true,
+          position: 'inside'
+        },
+        data: teacher.degree // Assuming 'scores' is an array of data for each teacher
+      });
+    });
+    return seriesData;
   }
 
   logOut() {
@@ -51,7 +103,7 @@ export class ChartPage implements OnInit {
     yAxis: [
       {
         type: 'category',
-        axisTick: {show: false},
+        axisTick: { show: false },
         data: []
       }
     ],
